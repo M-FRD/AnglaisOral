b9cbe3138bf607c2f2ba4ad7f0642e5a2eb13c09
M-FRD <86618885+M-FRD@users.noreply.github.com>
2022-11-02 17:05:46 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,30 +1,5 @@
-const compteurMael = document.querySelector('.MaelC')
-const compteurEvan = document.querySelector('.EvanC')
-const compteurMax = document.querySelector('.MaxC')
-const Mael = document.querySelectorAll('.Mael')
-const Evan = document.querySelectorAll('.Evan')
-const Max = document.querySelectorAll('.Max')
 const codecouleur = document.querySelector('.codecouleur')
 let lastscrolltop = 0
-
-function compteur(phrases, comp){
-    let compteur = 0
-    let texte = ''
-    phrases.forEach(phrase => { 
-        texte += ' ' + phrase.innerText
-    });
-    texte = texte.split(' ')
-    texte.forEach(mot => {
-        if(mot != ""){
-            compteur+=1
-        }
-    });
-   comp.innerText = `(${compteur} mots)`
-}
-
-compteur(Mael, compteurMael)
-compteur(Max, compteurMax)
-compteur(Evan, compteurEvan)
 
 // scroll
 
